ac19d4cbac41e16a654395737aa894e52db15945
Kipngetich33 <kipngetich.ngeno333@gmail.com>
2021-10-21 19:37:54 +0300

    add new icons

--- mobile_money/config/desktop.py
+++ mobile_money/config/desktop.py
@@ -7,7 +7,7 @@ def get_data():
 		{
 			"module_name": "Mobile Money",
 			"color": "grey",
-			"icon": "octicon octicon-file-directory",
+			"icon": "octicon octicon-device-mobile",
 			"type": "module",
 			"label": _("Mobile Money")
 		}
